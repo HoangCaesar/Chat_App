dfa75aaf96d9c6cb2de6b50a7bfb00c082cb7d7b
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-18 14:28:24 +0200

    Webapp: add config

--- Webapp/src/main.tsx
+++ Webapp/src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+// Project Import
 import App from './App';
+
+// ==============================|| MAIN: SETTING-ROUTER-HELMET PROVIDER  ||============================== //
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <HelmetProvider>
+            <Router>
+                <App />
+            </Router>
+        </HelmetProvider>
     </React.StrictMode>
 );
