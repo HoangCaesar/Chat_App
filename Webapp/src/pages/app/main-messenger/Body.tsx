5dd46fafa8c0a8238b50c1f4c88337869ac292a3
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-19 16:32:54 +0200

    Webapp: page/MainApp/main messenger -- body: struct types of msg

--- Webapp/src/pages/app/main-messenger/Body.tsx
+++ Webapp/src/pages/app/main-messenger/Body.tsx
@@ -1,17 +1,41 @@
-import { Box, Stack } from '@mui/material';
+import { Box } from '@mui/material';
+
+// Project Import
+import { Chat_History } from '../../../data/chat_data';
 
 // ==============================|| MESSENGER: BODY ||============================== //
 
 const Body = () => {
-    return <Box
-    sx={{
-        flexGrow: 1,
-        width: '100%',
-        backgroundColor: '#000',
-    }}
->
-    Body
-</Box>;
+    return (
+        <Box p={3}>
+            {Chat_History.map((item) => {
+                switch (item.type) {
+                    case 'divider':
+                        return <>Timeline</>;
+
+                    case 'msg':
+                        switch (item.subtype) {
+                            case 'img':
+                                return <>Media Message</>;
+
+                            case 'doc':
+                                return <>Doc Message</>;
+                            case 'Link':
+                                return <>Link Message</>;
+
+                            case 'reply':
+                                return <>Reply Message</>;
+
+                            default:
+                                return <>Text Message</>;
+                        }
+
+                    default:
+                        return <></>;
+                }
+            })}
+        </Box>
+    );
 };
 
 export default Body;
