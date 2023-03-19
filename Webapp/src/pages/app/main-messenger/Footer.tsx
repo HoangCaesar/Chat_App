0eb89f46976a14e9a56f8ba2f39cefbcf5ad0bf4
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-19 20:04:19 +0200

    Webapp: page/MainApp/main messenger -- footer: done

--- Webapp/src/pages/app/main-messenger/Footer.tsx
+++ Webapp/src/pages/app/main-messenger/Footer.tsx
@@ -1,17 +1,213 @@
-import { Box, Stack } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+import { Box, Fab, IconButton, InputAdornment, Stack, TextField, Tooltip } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import {
+    Camera,
+    File,
+    Image,
+    LinkSimple,
+    PaperPlaneTilt,
+    Smiley,
+    Sticker,
+    User
+} from 'phosphor-react';
+
+// Project Import
+
+// Style
+const StyledInput = styled(TextField)(({ theme }) => ({
+    '& .MuiInputBase-input': {
+        paddingTop: '12px !important',
+        paddingBottom: '12px !important',
+    },
+}));
 
 // ==============================|| MESSENGER: FOOTER ||============================== //
 
+const Actions = [
+    {
+        color: '#4da5fe',
+        icon: <Image size={24} />,
+        y: 102,
+        title: 'Photo/Video',
+    },
+    {
+        color: '#1b8cfe',
+        icon: <Sticker size={24} />,
+        y: 172,
+        title: 'Stickers',
+    },
+    {
+        color: '#0172e4',
+        icon: <Camera size={24} />,
+        y: 242,
+        title: 'Image',
+    },
+    {
+        color: '#0159b2',
+        icon: <File size={24} />,
+        y: 312,
+        title: 'Document',
+    },
+    {
+        color: '#013f7f',
+        icon: <User size={24} />,
+        y: 382,
+        title: 'Contact',
+    },
+];
+
+const ChatInput = ({ openPicker, setOpenPicker, setValue, value, inputRef }: any) => {
+    const [openActions, setOpenActions] = useState(false);
+
+    return (
+        <StyledInput
+            inputRef={inputRef}
+            value={value}
+            onChange={(event) => {
+                setValue(event.target.value);
+            }}
+            fullWidth
+            placeholder="Write a message..."
+            variant="filled"
+            InputProps={{
+                disableUnderline: true,
+                startAdornment: (
+                    <Stack sx={{ width: 'max-content' }}>
+                        <Stack
+                            sx={{
+                                position: 'relative',
+                                display: openActions ? 'inline-block' : 'none',
+                            }}
+                        >
+                            {Actions.map((el) => (
+                                <Tooltip placement="right" title={el.title}>
+                                    <Fab
+                                        onClick={() => {
+                                            setOpenActions(!openActions);
+                                        }}
+                                        sx={{
+                                            position: 'absolute',
+                                            top: -el.y,
+                                            backgroundColor: el.color,
+                                        }}
+                                        aria-label="add"
+                                    >
+                                        {el.icon}
+                                    </Fab>
+                                </Tooltip>
+                            ))}
+                        </Stack>
+
+                        <InputAdornment>
+                            <IconButton
+                                onClick={() => {
+                                    setOpenActions(!openActions);
+                                }}
+                            >
+                                <LinkSimple />
+                            </IconButton>
+                        </InputAdornment>
+                    </Stack>
+                ),
+                endAdornment: (
+                    <Stack sx={{ position: 'relative' }}>
+                        <InputAdornment>
+                            <IconButton
+                                onClick={() => {
+                                    setOpenPicker(!openPicker);
+                                }}
+                            >
+                                <Smiley />
+                            </IconButton>
+                        </InputAdornment>
+                    </Stack>
+                ),
+            }}
+        />
+    );
+};
+
+function linkify(text: string) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, (url) => `<a href="${url}" target="_blank">${url}</a>`);
+}
+
+function containsUrl(text: string) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return urlRegex.test(text);
+}
+
 const Footer = () => {
+    const theme = useTheme();
+
+    const [openPicker, setOpenPicker] = React.useState(false);
+
+    const [value, setValue] = useState('');
+    const inputRef = useRef(null);
+
     return (
         <Box
             sx={{
-                height: 100,
-                width: '100%',
-                backgroundColor: '#fff',
+                position: 'relative',
+                backgroundColor: 'transparent !important',
             }}
         >
-            Footer
+            <Box
+                p={2}
+                width={'100%'}
+                sx={{
+                    backgroundColor:
+                        theme.palette.mode === 'light'
+                            ? '#F8FAFF'
+                            : theme.palette.background.default,
+                    boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+                }}
+            >
+                <Stack direction="row" alignItems={'center'} spacing={3}>
+                    <Stack sx={{ width: '100%' }}>
+                        <Box
+                            style={{
+                                zIndex: 10,
+                                position: 'fixed',
+                                display: openPicker ? 'inline' : 'none',
+                                bottom: 81,
+                                right: 420,
+                            }}
+                        >
+                            <Picker theme={theme.palette.mode} data={data} />
+                        </Box>
+                        {/* Chat Input */}
+                        <ChatInput
+                            inputRef={inputRef}
+                            value={value}
+                            setValue={setValue}
+                            openPicker={openPicker}
+                            setOpenPicker={setOpenPicker}
+                        />
+                    </Stack>
+                    <Box
+                        sx={{
+                            height: 48,
+                            width: 48,
+                            backgroundColor: theme.palette.primary.main,
+                            borderRadius: 1.5,
+                        }}
+                    >
+                        <Stack
+                            sx={{ height: '100%' }}
+                            alignItems={'center'}
+                            justifyContent="center"
+                        >
+                            <IconButton>
+                                <PaperPlaneTilt color="#ffffff" />
+                            </IconButton>
+                        </Stack>
+                    </Box>
+                </Stack>
+            </Box>
         </Box>
     );
 };
