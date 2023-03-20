00f79973d7fc601f0eda73d74231f68d1887b993
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-20 11:12:19 +0200

    Webapp: update body of messenger

--- Webapp/src/pages/app/main-messenger/Body.tsx
+++ Webapp/src/pages/app/main-messenger/Body.tsx
@@ -17,7 +17,7 @@ const Body = ({ menu }: any) => {
                 overflowY: 'scroll',
 
                 backgroundColor:
-                    theme.palette.mode === 'light' ? '#F0F4FA' : theme.palette.background.default,
+                    theme.palette.mode === 'light' ? '#F0F4FA' : theme.palette.background.paper,
 
                 boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
             }}
