466dc589efd3e274e2823a04cb6e307cf0fbb41b
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-20 11:06:25 +0200

    Webapp: update body of messenger

--- Webapp/src/pages/app/main-messenger/Body.tsx
+++ Webapp/src/pages/app/main-messenger/Body.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Stack, useTheme } from '@mui/material';
 
 // Project Import
 import { Chat_History } from '../../../data/chat_data';
@@ -7,53 +7,68 @@ import { Timeline, DocMsg, LinkMsg, MediaMsg, ReplyMsg, TextMsg } from '../../..
 // ==============================|| MESSENGER: BODY ||============================== //
 
 const Body = ({ menu }: any) => {
+    const theme = useTheme();
     return (
-        <Box width="100%" sx={{ flexGrow: 1, height: '100%', overflowY: 'scroll' }}>
+        <Box
+            width="100%"
+            sx={{
+                position: 'relative',
+                flexGrow: 1,
+                overflowY: 'scroll',
+
+                backgroundColor:
+                    theme.palette.mode === 'light' ? '#F0F4FA' : theme.palette.background.default,
+
+                boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+            }}
+        >
             <Box p={3}>
-                {Chat_History.map((item) => {
-                    switch (item.type) {
-                        case 'divider':
-                            return (
-                                // Timeline
-                                <Timeline key={item.message} el={item} />
-                            );
+                <Stack spacing={1}>
+                    {Chat_History.map((item) => {
+                        switch (item.type) {
+                            case 'divider':
+                                return (
+                                    // Timeline
+                                    <Timeline key={item.message} el={item} />
+                                );
 
-                        case 'msg':
-                            switch (item.subtype) {
-                                case 'img':
-                                    return (
-                                        // Media Message
-                                        <MediaMsg key={item.message} el={item} menu={menu} />
-                                    );
+                            case 'msg':
+                                switch (item.subtype) {
+                                    case 'img':
+                                        return (
+                                            // Media Message
+                                            <MediaMsg key={item.message} el={item} menu={menu} />
+                                        );
 
-                                case 'doc':
-                                    return (
-                                        // Doc Message
-                                        <DocMsg key={item.message} el={item} menu={menu} />
-                                    );
-                                case 'Link':
-                                    return (
-                                        //  Link Message
-                                        <LinkMsg key={item.message} el={item} menu={menu} />
-                                    );
+                                    case 'doc':
+                                        return (
+                                            // Doc Message
+                                            <DocMsg key={item.message} el={item} menu={menu} />
+                                        );
+                                    case 'Link':
+                                        return (
+                                            //  Link Message
+                                            <LinkMsg key={item.message} el={item} menu={menu} />
+                                        );
 
-                                case 'reply':
-                                    return (
-                                        //  ReplyMessage
-                                        <ReplyMsg key={item.message} el={item} menu={menu} />
-                                    );
+                                    case 'reply':
+                                        return (
+                                            //  ReplyMessage
+                                            <ReplyMsg key={item.message} el={item} menu={menu} />
+                                        );
 
-                                default:
-                                    return (
-                                        // Text Message
-                                        <TextMsg key={item.message} el={item} menu={menu} />
-                                    );
-                            }
+                                    default:
+                                        return (
+                                            // Text Message
+                                            <TextMsg key={item.message} el={item} menu={menu} />
+                                        );
+                                }
 
-                        default:
-                            return <></>;
-                    }
-                })}
+                            default:
+                                return <></>;
+                        }
+                    })}
+                </Stack>
             </Box>
         </Box>
     );
