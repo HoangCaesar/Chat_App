96ac895e7406bf14fbe2eb94ebb87e3f9bd98ef8
Hoang <hoang.nguyen@kaalilogic.com>
2023-03-21 14:40:06 +0200

    Webapp: config axios

---
+++ Webapp/src/api/config.axios.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+// Project import
+import { BASE_URL } from './constants.axios';
+
+// helpers
+const getToken = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    return accessToken;
+};
+
+// ==============================|| MAIN AXIOS - CONFIG AXIOS ||============================== //
+
+const axiosClient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+axiosClient.interceptors.request.use((config: AxiosRequestConfig | any) => {
+    return {
+        ...config,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${getToken()}`,
+        },
+    };
+});
+
+axiosClient.interceptors.response.use(
+    (response: AxiosResponse) => {
+        if (response && response.data) {
+            return response.data;
+        }
+
+        return response;
+    },
+    (error) => {
+        return error;
+    }
+);
+
+export default axiosClient;
